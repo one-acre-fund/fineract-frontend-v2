e0fbf5dd73b2b295eb8ec43a687b44763a720163
Saurav P <testtingwork@gmail.com>
2022-10-13 13:38:00 +0530

    quick fix and linting

--- src/app/system/configurations/global-configurations-tab/clone-configuration/clone-configuration.component.ts
+++ src/app/system/configurations/global-configurations-tab/clone-configuration/clone-configuration.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { OrganizationService } from 'app/organization/organization.service';
-import { SystemService } from 'app/system/system.service';
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { ActivatedRoute, Router } from '@angular/router'
+import { OrganizationService } from 'app/organization/organization.service'
+import { SystemService } from 'app/system/system.service'
 
 @Component({
   selector: 'mifosx-clone-configuration',
@@ -10,16 +10,14 @@ import { SystemService } from 'app/system/system.service';
   styleUrls: ['./clone-configuration.component.scss']
 })
 export class CloneConfigurationComponent implements OnInit {
-
-
   /** Global Configuration form. */
-  configurationForm: FormGroup;
+  configurationForm: FormGroup
   /** Configuration. */
-  configuration: any;
+  configuration: any
 
-  listCountries: any = [];
-  listCountriesSliced: any = [];
-  configId: number;
+  listCountries: any = []
+  listCountriesSliced: any = []
+  configId: number
   /**
    * Retrieves the configuration data from `resolve`.
    * @param {FormBuilder} formBuilder Form Builder.
@@ -27,56 +25,60 @@ export class CloneConfigurationComponent implements OnInit {
    * @param {ActivatedRoute} route Activated Route.
    * @param {Router} router Router for navigation.
    */
-  constructor(private formBuilder: FormBuilder,
-              private systemService: SystemService,
-              private route: ActivatedRoute,
-              private router: Router,
-              private organizationService: OrganizationService) {
+  constructor (
+    private formBuilder: FormBuilder,
+    private systemService: SystemService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private organizationService: OrganizationService
+  ) {
     this.route.data.subscribe((data: { configuration: any }) => {
-      this.configuration = data.configuration;
-      this.configId = this.configuration?.id;
-    });
-    this.getCountries();
+      this.configuration = data.configuration
+      this.configId = this.configuration?.id
+    })
+    this.getCountries()
   }
 
   /**
    * Creates and sets the configuration form.
    */
-  ngOnInit() {
-    this.createConfigurationForm();
+  ngOnInit () {
+    this.createConfigurationForm()
   }
 
   /**
    * Creates and sets the global configuration form.
    */
-  createConfigurationForm() {
+  createConfigurationForm () {
     this.configurationForm = this.formBuilder.group({
-      'value': ['', Validators.required],
-      'countryId': [null, Validators.required],
-      'globalConfigId': [this.configId]
-    });
+      value: ['', Validators.required],
+      countryId: [null, Validators.required],
+      globalConfigId: [this.configId]
+    })
   }
 
-  getCountries() {
-    this.organizationService.getCountries().subscribe((res) => {
-      this.listCountries = res;
-      this.listCountriesSliced = this.listCountries;
-    });
+  getCountries () {
+    this.organizationService.getCountries().subscribe(res => {
+      this.listCountries = res
+      this.listCountriesSliced = this.listCountries
+    })
   }
 
-  public isFiltered(country: any) {
-    return this.listCountriesSliced.find(item => item.id === country.id);
+    /**
+   * filters the dropdown based on the search criteria
+   */
+  public isFiltered (country: any) {
+    return this.listCountriesSliced.find(item => item.id === country.id)
   }
   /**
    * Submits the global configuration form and updates global configuration,
    * if successful redirects to view all global configurations.
    */
-  submit() {
+  submit () {
     this.systemService
       .cloneConfiguration(this.configuration.id, this.configurationForm.value)
       .subscribe((response: any) => {
-        this.router.navigate(['../../'], { relativeTo: this.route });
-      });
+        this.router.navigate(['../../'], { relativeTo: this.route })
+      })
   }
-
 }
