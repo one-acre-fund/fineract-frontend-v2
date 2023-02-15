c348d793eb1c33ffa9e78b644e6ac686ee0212dd
Gilbert Adjin Frimpong <gilclefes@gmail.com>
2023-02-15 14:10:26 +0200

    remove package-lock.json and .env.ts

---
+++ src/environments/.env.ts
@@ -0,0 +1,10 @@
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+export default {
+  'mifos_x': {
+    'version': '0.0.0',
+    'hash': 'dccc2721'
+  },
+  'allow_switching_backend_instance': true
+};
+/* tslint:enable */
