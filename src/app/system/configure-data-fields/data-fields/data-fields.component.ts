071d61fd86a786d52fed083ff1e5c25d72a25a84
SauravPriyadarshy <108924123+SauravPriyadarshy@users.noreply.github.com>
2023-04-04 13:04:34 +0530

    changes for adding back button on list

--- src/app/system/configure-data-fields/data-fields/data-fields.component.ts
+++ src/app/system/configure-data-fields/data-fields/data-fields.component.ts
@@ -37,20 +37,21 @@ export class DataFieldsComponent implements OnInit {
   ];
   columnTypeListFiltered: any = [];
 
-  displayedColumns = ["field", "isEnabled", "isMandatory", "isUnique","actions"];
-  addDisplayedColumns = ["name", "type", "mandatory", "isUnique", "length","code","actions"];
+  displayedColumns = ["field", "isEnabled", "isMandatory", "isUnique", "actions"];
+  addDisplayedColumns = ["name", "type", "mandatory", "isUnique", "length", "code", "actions"];
 
   addDataSource = new BehaviorSubject<AbstractControl[]>([]);
   rows: FormArray = this.fb.array([]);
   form: FormGroup = this.fb.group({ tableArray: this.rows });
   @ViewChild(NgModel, { static: true }) addField: NgModel;
+  toggleMatTable: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
     private systemService: SystemService,
     private router: Router,
     private fb: FormBuilder,
-    private alertService:AlertService
+    private alertService: AlertService
   ) {
     this.route.data.subscribe((data: { columnCodes: any }) => {
       this.codes = data.columnCodes;
@@ -98,16 +99,15 @@ export class DataFieldsComponent implements OnInit {
     }
   }
 
-  subEntity:any;
+  subEntity: any;
   selectedSubModule(subEntity: any) {
-    this.subEntity=subEntity;
+    this.subEntity = subEntity;
     this.datatableName = "m_field_" + subEntity.toLowerCase() + "_" + this.type.charAt(0).toLowerCase();
     if (subEntity && this.subModule.length > 0) {
       this.updatedFieldsData = this.subModuleFields.filter((i) => i.subentity === subEntity);
     }
   }
 
-  toggleMatTable: boolean = false;
   addControl() {
     if (this.fieldName && this.columnType) {
       this.toggleMatTable = true;
@@ -116,15 +116,15 @@ export class DataFieldsComponent implements OnInit {
         type: [this.columnType],
         mandatory: [false],
         isUnique: [false],
-        length: new FormControl({value: null, disabled: this.columnType!=='String'}),
-        code:new FormControl({value: null, disabled: this.columnType!=='Dropdown'}),
-        subEntity:this.subEntity,
-        entity:this.type
+        length: new FormControl({ value: null, disabled: this.columnType !== "String" }),
+        code: new FormControl({ value: null, disabled: this.columnType !== "Dropdown" }),
+        subEntity: this.subEntity,
+        entity: this.type,
       });
       this.rows.push(row);
       this.updateView();
-      this.fieldName=null;
-      this.columnType=null;
+      this.fieldName = null;
+      this.columnType = null;
       this.addField.control.clearValidators();
     } else {
       this.toggleMatTable = false;
@@ -135,77 +135,87 @@ export class DataFieldsComponent implements OnInit {
     this.addDataSource.next(this.rows.controls);
   }
 
-  removeNode(index:number,flag=false){
-    const control = this.form.get('tableArray') as FormArray;
+  removeNode(index: number, flag = false) {
+    const control = this.form.get("tableArray") as FormArray;
     control.removeAt(index);
     this.updateView();
   }
 
-  changeType(index:number,value:any){
-    const controls = this.form.get('tableArray') as FormArray;
-    if(controls){
-      let control:any=controls.controls[index];
-      control.value.type=value;
-      if(value!=='String'){
-        control.controls['length'].disable();
-      }else{
-        control.controls['length'].enable();
+  changeType(index: number, value: any) {
+    const controls = this.form.get("tableArray") as FormArray;
+    if (controls) {
+      let control: any = controls.controls[index];
+      control.value.type = value;
+      if (value !== "String") {
+        control.controls["length"].disable();
+      } else {
+        control.controls["length"].enable();
       }
 
-      if(value!=='Dropdown'){
-        control.controls['code'].disable();
-      }else{
-        control.controls['code'].enable();
+      if (value !== "Dropdown") {
+        control.controls["code"].disable();
+      } else {
+        control.controls["code"].enable();
       }
     }
     this.updateView();
   }
 
-  saveControls(){
-    let value=this.form.get('tableArray').value;
+  saveControls() {
+    let value = this.form.get("tableArray").value;
 
-    let model={
-      apptableName:`m_${this.type.toLowerCase()}`,
-      datatableName:this.datatableName,
-      officeCountryId:this.countryId,
+    let model = {
+      apptableName: `m_${this.type.toLowerCase()==='loantype'?'loan':this.type.toLowerCase()==='ou'?'office':this.type.toLowerCase()}`,
+      datatableName: this.datatableName,
+      officeCountryId: this.countryId,
       addColumns:value
-    }
+    };
 
-    this.systemService.saveFieldConfiguration(model).subscribe((res:any)=>{
-      this.rows.clear();
-      this.getFieldConfigurationByType();
-      this.toggleMatTable=false;
-    },err=>{
-      this.alertService.alert({
-        type: 'Error while saving data',
-        message:  err?.error?.errors?err?.error?.errors[0]?.developerMessage:err?.error?.error
+    this.systemService.saveFieldConfiguration(model).subscribe(
+      (res: any) => {
+        this.rows.clear();
+        this.getFieldConfigurationByType();
+        this.toggleMatTable = false;
+      },
+      (err) => {
+        this.alertService.alert({
+          type: "Error while saving data",
+          message: err?.error?.errors ? err?.error?.errors[0]?.developerMessage : err?.error?.error,
+        });
+      }
+    );
+  }
+
+  updateControl(value: any) {
+    let model = {
+      apptableName: `m_${this.type.toLowerCase()}`,
+      datatableName: this.datatableName,
+      field: value.field,
+      isEnabled: value.isEnabled,
+      isMandatory: value.isMandatory,
+      isUnique: value.isUnique,
+    };
+    this.systemService
+      .updateFieldConfiguration(model, value.fieldConfigurationId, this.countryId)
+      .subscribe((res: any) => {
+        this.getFieldConfigurationByType();
+        this.toggleMatTable = false;
       });
-    })
   }
 
-  updateControl(value:any){
-    let model={
-      apptableName:`m_${this.type.toLowerCase()}`,
-      datatableName:this.datatableName,
-      field:value.field,
-      isEnabled:value.isEnabled,
-      isMandatory:value.isMandatory,
-      isUnique:value.isUnique
-    }
-    this.systemService.updateFieldConfiguration(model,value.fieldConfigurationId,this.countryId).subscribe((res:any)=>{
+  deleteRecord(field: any, fieldConfigurationId: any) {
+    let model = {
+      apptableName: `m_${this.type.toLowerCase()}`,
+      datatableName: this.datatableName,
+    };
+    this.systemService.deleteConfiguration(fieldConfigurationId, model).subscribe((res: any) => {
       this.getFieldConfigurationByType();
-      this.toggleMatTable=false;
-    })
+      this.toggleMatTable = false;
+    });
   }
 
-  deleteRecord(field:any, fieldConfigurationId:any){
-    let model={
-      apptableName:`m_${this.type.toLowerCase()}`,
-      datatableName:this.datatableName
-    }
-    this.systemService.deleteConfiguration(fieldConfigurationId,model).subscribe((res:any)=>{
-      this.getFieldConfigurationByType();
-      this.toggleMatTable=false;
-    })
+  goBack() {
+    this.rows.clear();
+    this.toggleMatTable = false;
   }
 }
