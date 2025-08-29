51e9b539cc5a90ebdbacff699edb3e0e52fac314
Daniel Simiyu <daniel.simiyu@oneacrefund.org>
2025-08-29 17:01:28 +0300

    CP-3518 refactoring

--- src/app/tasks/checker-inbox-and-tasks-tabs/loan-approval-tabs/loan-approval-tabs.component.ts
+++ src/app/tasks/checker-inbox-and-tasks-tabs/loan-approval-tabs/loan-approval-tabs.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'mifosx-loan-approval-tabs',
   templateUrl: './loan-approval-tabs.component.html',
   styleUrls: ['./loan-approval-tabs.component.scss']
 })
-export class LoanApprovalTabsComponent implements OnInit {
+export class LoanApprovalTabsComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
