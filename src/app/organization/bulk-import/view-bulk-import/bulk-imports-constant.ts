8f49252d35d41b489fa8e3d921989ceaaa523f81
amy.muhimpundu <amy.muhimpundu@oneacrefund.org>
2026-04-15 11:51:57 +0200

    (CP-4085) Add missing semicolons

--- src/app/organization/bulk-import/view-bulk-import/bulk-imports-constant.ts
+++ src/app/organization/bulk-import/view-bulk-import/bulk-imports-constant.ts
@@ -4,9 +4,9 @@ export class BulkImportsConstants {
   static readonly CLIENT_TRANSFER_IMPORT = "Client Transfer";
   static readonly SAVINGS_TRANSACTIONS_IMPORT = "Savings Transactions";
   static readonly USERS_IMPORT = "Users";
-  static readonly GROUPS_IMPORT = "Groups"
+  static readonly GROUPS_IMPORT = "Groups";
   static readonly CENTERS_IMPORT = "Centers";
-  static readonly CLIENTS_IMPORT = "Clients"
+  static readonly CLIENTS_IMPORT = "Clients";
   static readonly LOAN_REPAYMENTS_IMPORT = "Loan Repayments";
   static readonly ACCOUNT_TRANSFER_TRANSACTION_IMPORT = "Account Transfer Transaction";
   static readonly GROUP_OFFICE_TRANSFER_IMPORT = "Group Office Transfer";
